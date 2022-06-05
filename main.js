ff3700d02d676c0ccb1dc4a69d019ef87379f854
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-05 21:32:26 +0400

    testing

---
+++ main.js
@@ -0,0 +1,44 @@
+const input = document.getElementById("input");
+const par = document.getElementById("p");
+const keyCodes = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+input.addEventListener("keyup", (event) => {
+  if (keyCodes.includes(event.key)) p.innerText += event.key;
+});
+x;
