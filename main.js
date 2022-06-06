3983aa010029dcd9c7dd7b6e89b880619c589af1
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-07 00:10:35 +0400

    testing color change on typing

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ let trainingText = document.getElementById("training-text");
 const resultText = document.getElementById("paragraph");
 let count = 0;
 trainingText.innerText = "Hello world";
+
+
 inputText.addEventListener("keypress", (event) => {
   if (event.key == trainingText.innerText[count]) {
+    // trainingText.innerHTML = trainingText.innerHTML.replace(
+    //   trainingText.innerText[count],
+    //   '<span style="color: red;">' + trainingText.innerText[count] + "</span>"
+    // );
+    console.log(trainingText.innerText[count]);
     inputText.value += event.key;
     resultText.innerText = inputText.value;
     count++;
-    console.log(event.key == " ");
   }
   event.preventDefault();
 });
