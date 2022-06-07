fa5eec563a5afb886fe3a30cf1d8a86dec59c042
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-08 00:10:23 +0400

    update

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const inputText = document.getElementById("input");
+const divs = document.querySelectorAll("div");
 let trainingText = document.getElementById("training-text");
 let count = 0;
-trainingText.innerText = "Hello world";
 
+trainingText.innerText = "hello world";
 
 inputText.addEventListener("keypress", (event) => {
   if (event.key == trainingText.innerText[count]) {
@@ -10,6 +11,13 @@ inputText.addEventListener("keypress", (event) => {
     //   trainingText.innerText[count],
     //   '<span style="color: red;">' + trainingText.innerText[count] + "</span>"
     // );
+    for (let i = 0; i < divs.length; i++) {
+      if (event.key == divs[i].innerText) {
+        divs[i].style.backgroundColor = "red";
+        divs[i].style.color = "white";
+        divs[i].style.borderColor = "gray";
+      }
+    }
     inputText.value += event.key;
     count++;
   }
