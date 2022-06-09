df00773b6057c1cba88284019fabb6af2329a326
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-09 23:06:09 +0400

    added styles to key release state

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ let trainingText = document.getElementById("training-text");
 let mistakesCount = 0;
 let count = 0;
 
-//Setting up some training text, 
+//Setting up some training text,
 //but it is better to fetch some text from online resources in the future
 trainingText.innerText = "hello world";
 
 //Main event listener
-inputText.addEventListener("keypress", (event) => {
+inputText.addEventListener("keydown", (event) => {
   if (event.key == trainingText.innerText[count]) {
     //Changing the color of training text if it is typed right
     trainingText.innerHTML =
@@ -18,6 +18,7 @@ inputText.addEventListener("keypress", (event) => {
       trainingText.innerText.slice(0, inputText.value.length + 1) +
       "</span>" +
       trainingText.innerText.slice(inputText.value.length + 1);
+
     //Changing the color of pressed keys on the virtual keyboard
     for (let i = 0; i < divs.length; i++) {
       if (event.key == divs[i].innerText) {
@@ -28,18 +29,31 @@ inputText.addEventListener("keypress", (event) => {
     }
     inputText.value += event.key;
     count++;
-  } 
+  }
+
   //Counting mistakes
   else if (event.key != trainingText.innerText[count]) {
     mistakesCount++;
     mistakesPlace.innerText = `Mistakes: ${mistakesCount}`;
   }
+
   //The results. It should designed better rather than a simple alert box!
   if (trainingText.innerText == inputText.value) {
     alert(`You did it!\nYou have ${mistakesCount} mistakes`);
     location.reload();
   }
   event.preventDefault();
+});
+
+//Styles when keys are up
+inputText.addEventListener("keyup", (event) => {
+  for (let i = 0; i < divs.length; i++) {
+    if (event.key == divs[i].innerText) {
+      divs[i].style.backgroundColor = "white";
+      divs[i].style.color = "black";
+      divs[i].style.borderColor = "black";
+    }
+  }
 });
 
 //Clearing input on reload
