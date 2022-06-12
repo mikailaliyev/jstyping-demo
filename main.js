c9c096753c0506f0f379b9c97f4ac097d7a5cc41
mikailaliyev <mikail.aliyev@gmail.com>
2022-06-12 23:28:56 +0400

    update

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const inputText = document.getElementById("input");
 const divs = document.querySelectorAll("div");
 const mistakesPlace = document.getElementById("mistakes");
 const typingSpeed = document.getElementById("typing-speed");
-
 const keyCodes = [
   " ",
   "a",
@@ -32,6 +31,8 @@ const keyCodes = [
   "x",
   "y",
   "z",
+  ".",
+  ",",
 ];
 let trainingText = document.getElementById("training-text");
 let mistakesCount = 0;
@@ -40,7 +41,7 @@ let allKeysSum = 0;
 
 //Setting up some training text,
 //but it is better to fetch some text from online resources in the future
-trainingText.innerText = "hello world brothers how are you doing?";
+trainingText.innerText = "hello world brothers how are you doing.";
 
 //Main event listener
 inputText.addEventListener("keydown", (event) => {
@@ -50,8 +51,11 @@ inputText.addEventListener("keydown", (event) => {
     alert("Not US Layout");
   }
   if (event.key == trainingText.innerText[count]) {
-    allKeysSum++;
-    typingSpeed.innerText = `Speed: ${allKeysSum / 5 / getSec / 60}`;
+    event.key == " " ? allKeysSum++ : allKeysSum;
+    console.log(allKeysSum, getSec);
+    typingSpeed.innerText = `Speed: ${Math.ceil(
+      allKeysSum / (getSec / 60)
+    )} wps`;
     //Changing the color of training text if it is typed right
     trainingText.innerHTML =
       "<span style = 'color: green'>" +
